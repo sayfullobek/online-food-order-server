d7c66812080aa674d673e86d6afed17ee2f83130
sayfullobek__ <107360172+sayfullobek@users.noreply.github.com>
2024-01-26 03:54:17 -0800

    v1

---
+++ src/main/java/it/revo/Online/food/order/OnlineFoodOrderApplication.java
@@ -0,0 +1,13 @@
+package it.revo.Online.food.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineFoodOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineFoodOrderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/revo/Online/food/order/OnlineFoodOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package it.revo.Online.food.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineFoodOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
